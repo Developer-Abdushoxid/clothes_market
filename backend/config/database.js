1b4cb3f13d6b285c020a870afb45bb1fb1732dd5
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-05-29 18:56:19 +0500

    navbar added

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool(process.env.DATABASE_URL,{
-    dialect: 'postgres',
-    logging: false,
-});
-
-module.exports = pool;
