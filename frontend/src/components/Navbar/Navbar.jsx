358f872c0e422b72603ab92200d053fb63970bf9
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-05-30 22:07:42 +0500

    added search

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { MdSearch } from "react-icons/md";
+import { FaShoppingCart } from "react-icons/fa";
+import DarkMode from './DarkMode';
+
 
 const MenuLinks = [
     {
@@ -27,7 +31,7 @@ const Navbar = () => {
     return (
         <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
             <div className='py-4'>
-                <div className="container">
+                <div className="container flex justify-between items-center">
                     {/* Logo and Links section */}
                     <div className="flex  items-center gap-4">
                         <a href="#"
@@ -51,8 +55,28 @@ const Navbar = () => {
                             </ul>
                         </div>
                     </div>
+
                     {/* Navbar Right section */}
+                    <div className='flex justify-between items-center gap-4'>
+                {/* search bar section */}
+                <div className='relative group hidden sm:block'>
+                   <input type="text" placeholder='Search'
+                   className='search-bar' /> 
+                   <MdSearch  className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400
+                   absolute top-1/2 -translate-y-1/2 right-3 duration-200'/>
                 </div>
+                {/* order button section */}
+                <button className='relative p-3'>
+                <FaShoppingCart className='text-xl text-gray-600 dark:text-gray-400' />
+                <div className='w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 
+                flex items-center justify-center text-xs'>4</div>
+                </button>
+                {/* dark mode section */}
+                <div>
+                    <DarkMode />
+                </div>
+                </div>
+              </div>
             </div>
         </div>
     )
