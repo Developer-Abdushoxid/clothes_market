5eea3a17bb00abd847e6834e8d2342ca8ac5181f
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-06-02 14:36:03 +0500

    main added

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -3,6 +3,7 @@ import Slider from "react-slick";
 import Image1 from '../../assets/hero/headphone.png'
 import Image2 from '../../assets/category/vr.png'
 import Image3 from '../../assets/category/macbook.png'
+import Button from '../Shared/Button';
 
 const HeroData =[
     {
@@ -22,7 +23,7 @@ const HeroData =[
     
     },
     {
-        id:3,
+        id:2,
         img: Image3,
         subtitle: 'Beats Solo',
         title: 'Branded',
@@ -44,10 +45,44 @@ const Hero = () => {
         pauseOnFocus: true,
     };
   return (
-    <div>
-        <div className=''>
+    <div className='container'>
+        <div className='overflow-hidden rounded-3xl min-h-[550px]
+        sm:min-h-[650px] hero-bg-color flex justify-center items-center'>
+
+            <div className='container pb-8 sm:pb-0'>
             {/* hero section */}
-            <Slider {... settings}> </Slider>
+            <Slider {...settings}>
+            {HeroData.map((data) => (
+                <div key={data.id}>
+                    <div className='grid grid-cols-1 sm:grid-cols-2'>
+                        {/* text content section */}
+                        <div className='flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 
+                        text-center sm:text-left order-2 sm:order-1 relative z-10'>
+                            <h1 className='text-2xl sm:text-6xl lg:text-2xl font-bold'>{data.subtitle}</h1>
+                            <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                            <h1 className='text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px]
+                            xl:text-[150px] font-bold'>{data.title2}</h1>
+                            <div>
+                                <Button
+                                text='Shop By Category'
+                                bgColor='bg-primary'
+                                textColor='text-white'/>
+                            </div>
+                        </div>
+                        {/* img section */}
+                        <div className='order-1 sm:order-2'>
+                            <div>
+                                <img src={data.img}
+                                 alt=""
+                                 className='w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110
+                                 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+             </Slider>
+             </div>
         </div>
     </div>
   )
